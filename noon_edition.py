7d0032ecdf0f8ca07aa4c30605a7932c04f894f0
Ruben Kruepper <ruben.kruepper@gmail.com>
2021-03-30 10:25:56 +0200

    incorporated production schedule

--- noon_edition.py
+++ noon_edition.py
@@ -10,35 +10,35 @@ import datetime as dt
 import shutil
 import time
 
-shutil.rmtree(cfg.CHART_PATH, ignore_errors=True)
 
-API_KWARGS = {
-    'fcsapi'    : fcsapi(cfg.FSCAPI_TOKEN),
-    'fxsc'      : FxstreetCalendar(),
-    'ans'       : AzureNewsService(cfg.AZURE_KEY),
-    'cg'        : ChartGrabber(cfg.TRADINGVIEW_URL, cfg.TRADINGVIEW_USERNAME, cfg.TRADINGVIEW_PASSWORD, cfg.NOON_EDITION_CHART_PATH)
-}
-AUTHORS = [
-    (EnglishAuthor, 'Vicki Master - EN - Noon Edition'),
-    (JapaneseAuthor, 'Vicki Master - JP - Noon Edition'), 
-    (ThaiAuthor, 'Vicki Master - TH - Noon Edition'),
-    (ChineseAuthor, 'Vicki Master - CN - Noon Edition'),
-    (MalaysianAuthor, 'Vicki Master - MY - Noon Edition'),
-    (VietnameseAuthor, 'Vicki Master - VN - Noon Edition')
-]
+if not dt.datetime.today().weeekday() in (4,5):
+    API_KWARGS = {
+        'fcsapi'    : fcsapi(cfg.FSCAPI_TOKEN),
+        'fxsc'      : FxstreetCalendar(),
+        'ans'       : AzureNewsService(cfg.AZURE_KEY),
+        'cg'        : ChartGrabber(cfg.TRADINGVIEW_URL, cfg.TRADINGVIEW_USERNAME, cfg.TRADINGVIEW_PASSWORD, cfg.NOON_EDITION_CHART_PATH)
+    }
+    AUTHORS = [
+        (EnglishAuthor, 'Vicki Master - EN - Noon Edition'),
+        (JapaneseAuthor, 'Vicki Master - JP - Noon Edition'), 
+        (ThaiAuthor, 'Vicki Master - TH - Noon Edition'),
+        (ChineseAuthor, 'Vicki Master - CN - Noon Edition'),
+        (MalaysianAuthor, 'Vicki Master - MY - Noon Edition'),
+        (VietnameseAuthor, 'Vicki Master - VN - Noon Edition')
+    ]
 
 
-EnglishAuthor('Vicki Master - EN - Noon Edition', **API_KWARGS).write_headline_news()
+    EnglishAuthor('Vicki Master - EN - Noon Edition', **API_KWARGS).write_headline_news()
 
-def gen_authors(authors):
-    for author, workbook in authors:
-        print(workbook)
-        yield author(workbook, **API_KWARGS)
+    def gen_authors(authors):
+        for author, workbook in authors:
+            print(workbook)
+            yield author(workbook, **API_KWARGS)
 
-for author in gen_authors(AUTHORS):
-    author.write_stock_sentences()
-    author.write_forex_sentences()
-    author.write_economic_calendar(date=dt.datetime.today() + dt.timedelta(days=1))
-    time.sleep(30)
+    for author in gen_authors(AUTHORS):
+        author.write_stock_sentences()
+        author.write_forex_sentences()
+        author.write_economic_calendar(date=dt.datetime.today() + dt.timedelta(days=1))
+        time.sleep(30)
 
-API_KWARGS['cg'].driver.close()
+    API_KWARGS['cg'].driver.close()
